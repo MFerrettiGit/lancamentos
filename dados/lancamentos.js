b7b8eda2e9c04bb09854f2752bb8e15743ba15e6
MFerrettiGit <compras@mferretti.com.br>
2026-07-06 05:09:44 -0300

    Atualizacao automatica 06/07/2026 05:09

--- dados/lancamentos.js
+++ dados/lancamentos.js
@@ -1,5 +1,5 @@
 ﻿window.LANCAMENTOS = {
-  meta: { atualizadoEm: "05/07/2026 05:03", periodoDados: "01/2025 a 07/2026", metaClientes: 200, metaPorSetor: 15 },
+  meta: { atualizadoEm: "06/07/2026 05:03", periodoDados: "01/2025 a 07/2026", metaClientes: 200, metaPorSetor: 15 },
   produtos: [
   {
     codigo: "CREYAMAH2",
